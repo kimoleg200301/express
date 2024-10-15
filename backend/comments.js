45bc6357c190777d90cb28eb856afb6ab195736a
kimoleg200301 <kimoleg200301@gmail.com>
2024-10-16 03:26:26 +0500

    the authorization problem will be solved

--- backend/comments.js
+++ backend/comments.js
@@ -2,13 +2,18 @@ const express = require('express');
 const mysql = require('mysql2/promise');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const port = 3000;
 const ip = '192.168.31.245';
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost', // ваш клиентский домен
+  credentials: true // позволяет передавать cookies
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 const pool = mysql.createPool({
   host: 'localhost',
@@ -21,13 +26,14 @@ const pool = mysql.createPool({
 });
 
 function authenticateToken(req, res, next) {
-  const token = req.cookies.jwt_token || req.headers('Authorization');
+  const token = req.cookies.jwt_token || req.headers['Authorization'];
+  console.log(req.cookies.jwt_token);
 
   if (!token) {
     return res.status(401).json({ message: 'Токен не предоставлен!' });
   }
 
-  jwt.verify(token, 'mother', (err, decoded) => {
+  jwt.verify(token, 'qwerty', (err, decoded) => {
     if (err) {
       return res.status(403).json({ message: 'Токен не валидный!' });
     }
@@ -37,30 +43,10 @@ function authenticateToken(req, res, next) {
 
 }
 
-// con.connect((err) => {  
-//   if (err) { //'Ошибка поключения к базе данных: ', err.stack
-//     console.error(
-//       app.get("/result_to_connect_db", function(request, response) {
-//         const result = {
-//           message: `Ошибка подключения к базе данных: ${err.stack}`,
-//         }
-//         response.json(result);
-//       })
-//     );
-//     return; 
-//   }
-//   app.get("/result_to_connect_db", function(request, response) {
-//     const result = {
-//       message: 'Успешное подключение к базе данных!',
-//     }
-//     response.json(result);
-//   })
-// }); 
-
-app.get('/', async function(req, res) {
-  //место для написания запросов к базе данных для извлечения всех товаров. Ниже написан пример результата запросов из БД
+app.post('/', authenticateToken, async function(req, res) {
   const [data] = await pool.query(`select * from objects`);
   //const [review_] = await pool.query(`SELECT * FROM objects o INNER JOIN reviews r ON o.reviews_id = r.reviews_id;`);
+
 
   res.json(data[0]);
 });
@@ -79,21 +65,22 @@ async function getUser() {
       // })
 
       const [data] = await pool.query(`select * from users where name = ?`, [login]);
+
       if (data.length > 0) {
         if (data[0].password === password) {
-          res.json({
-            success: `Поздравляем! Вы успешно авторизованы!`,
-            login: `Ваш логин: ${login}`,
-            password: `Ваш пароль: ${password}`
-          });
-          // начало генерации токена
+          const token = jwt.sign({name: data[0].name, role: data[0].role}, 'qwerty', { expiresIn: '1h' });
+          console.log(`Token: ${token}`);
 
-          const token = jwt.sign({name: data[0].name}, 'mother', { expiresIn: '1h' });
-          if (req.headers['user-agent'].includes('Mozilla')) {
-            res.cookie('jwt_token', token, { httpOnly: true, sameSite: 'Strict' });
+          if (req.headers['user-agent'] && req.headers['user-agent'].includes('Mozilla')) {
+            res.cookie('jwt_token', token, { httpOnly: true, sameSite: 'Lax', path: '/' });
+            return res.json({
+              success: `Поздравляем! Вы успешно авторизованы!`,
+              login: `Ваш логин: ${login}`,
+              password: `Ваш пароль: ${password}`
+            });
           }
           else {
-            res.json({token});
+            return res.json({token});
           }
         }
         else {
@@ -158,7 +145,7 @@ async function addUser() {
       const [data_check] = await pool.query(`select name from users where name = ?`, [login])
 
       if (password === password_ || data_check[0] !== login) {
-        const [data] = await pool.query(`insert into users (name, password) values (?, ?)`, [login, password]);
+        const [data] = await pool.query(`insert into users (name, password, role) values (?, ?, ?)`, [login, password, 'client']);
         res.json({
           result: true,
           message: `Аккаунт создан. Авторизуйтесь под новым аккаунтом! ${data[0]}`,
